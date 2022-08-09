5659f78303c85cdaa56dd69e59fcc3fa66233608
rejennylo <rejennylo@gmail.com>
2022-08-09 16:53:12 +0800

    feat: add index pagination

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const BASE_URL = 'https://lighthouse-user-api.herokuapp.com/'
 const INDEX_URL = BASE_URL + 'api/v1/users/'
 const friends = []
+const FRIENDS_PER_PAGE = 12
 
 const dataPanel = document.querySelector('#data-panel')
+const paginatior = document.querySelector('#paginatior')
 
+// 渲染出好友
 function renderFriendList (data) {
-  let containerHTML = ''
+  let containerHTML = '' // 建立空字串, 用來儲存模板
   data.forEach((item) => {
     containerHTML += `
         <div class="card m-2" style="width: 18rem;">
@@ -19,9 +22,51 @@ function renderFriendList (data) {
   dataPanel.innerHTML = containerHTML
 }
 
+// 取出特定頁面資料
+function getFriendsByPage(page) {
+  const startIndex = (page-1) * FRIENDS_PER_PAGE // 計算起始 Index
+  return friends.slice(startIndex, startIndex + FRIENDS_PER_PAGE) // 回傳切割後的新陣列
+}
+
+// 渲染出分頁
+function renderPaginatior(amount) {
+  const numberOfPages = Math.ceil(amount / FRIENDS_PER_PAGE) //  輸入的值除以12, 並用ceil方法得到總頁數
+  let rawHTML = '' // 建立空字串, 用來儲存模板
+  
+  // 加上左邊箭頭
+  rawHTML += `<li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous" data-page-left="page-left">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>`
+
+  // 迴圈產生分頁編號
+  for (let page = 1; page <= numberOfPages; page ++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
+  }
+
+  // 加上右邊箭頭
+  rawHTML += `<li class="page-item">
+                <a class="page-link" href="#" aria-label="Next" data-page-right="page-right">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>`
+
+  paginatior.innerHTML = rawHTML // 放入 paginatior
+}
+
+// 新增監聽器在 paginatior 上
+paginatior.addEventListener('click', function onPaginatiorClicked(event) {
+  if (event.target.tagName !== 'A') return // 如果點擊到非目標就離開
+
+  const page = Number(event.target.dataset.page) // 透過 dataset 取得點擊頁數
+  renderFriendList(getFriendsByPage(page)) //更新名單
+})
+
 axios
   .get (INDEX_URL)
   .then ((res) => {
     friends.push(...res.data.results)
-    renderFriendList(friends)
+    renderPaginatior(friends.length) // 渲染分頁編號
+    renderFriendList(getFriendsByPage(1)) // 取得分頁資料後渲染名單 (預設從第1頁開始)
   })
